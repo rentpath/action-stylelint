6cfb19bd4ae9e0e80f5b519a3742d9b103642232
Tad Johnston <tjohnston@rentpath.com>
2020-05-07 10:22:00 -0700

    add name input
    * allows us to run the linter multiple times

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,8 @@ if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
   # Use jq and github-pr-review reporter to format result to include link to rule page.
   stylelint --allow-empty-input --ignore-pattern "${INPUT_STYLELINT_IGNORE}" "${INPUT_STYLELINT_INPUT:-'**/*.css'}" --config="${INPUT_STYLELINT_CONFIG}" -f json \
     | jq -r '.[] | {source: .source, warnings:.warnings[]} | "\(.source):\(.warnings.line):\(.warnings.column):\(.warnings.severity): \(.warnings.text) [\(.warnings.rule)](https://stylelint.io/user-guide/rules/\(.warnings.rule))"' \
-    | reviewdog -efm="%f:%l:%c:%t%*[^:]: %m" -name="stylelint" -reporter=github-pr-review -level="${INPUT_LEVEL}"
+    | reviewdog -efm="%f:%l:%c:%t%*[^:]: %m" -name="${INPUT_NAME}:-stylelint}" -reporter=github-pr-review -level="${INPUT_LEVEL}"
 else
   stylelint --allow-empty-input --ignore-pattern "${INPUT_STYLELINT_IGNORE}" "${INPUT_STYLELINT_INPUT:-'**/*.css'}" --config="${INPUT_STYLELINT_CONFIG}" \
-    | reviewdog -f="stylelint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+    | reviewdog -f="stylelint" -name="${INPUT_NAME:-stylelint}" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
 fi
